34e71514e67de110b39fcfbb4b334f404142d574
TheMasterWhite <2778178745@qq.com>
2024-06-25 21:43:36 +0800

    头文件

---
+++ utils/HeadFiles.py
@@ -0,0 +1,38 @@
+import datetime
+import os
+import time
+from configparser import ConfigParser
+import erniebot
+import gradio as gr
+import asyncio
+from erniebot_agent.memory import HumanMessage, AIMessage
+from erniebot_agent.chat_models import ERNIEBot
+import requests
+import base64
+import pathlib
+import json
+import oss2
+from oss2.credentials import EnvironmentVariableCredentialsProvider
+from pathlib import Path
+
+
+def ReadConfigFile():
+    # 读取配置文件
+    Config = ConfigParser()
+    Config.optionxform = str
+    Config.read('Config.cfg')
+    # 遍历配置文件中的所有选项，创建全局变量
+
+    global_vars = dict(Config['TOKENS'])
+    for key, value in global_vars.items():
+        globals()[key] = value
+
+
+ReadConfigFile()
+
+
+os.environ['OSS_ACCESS_KEY_ID'] = OSS_ACCESS_KEY_ID
+os.environ['OSS_ACCESS_KEY_SECRET'] = OSS_ACCESS_KEY_SECRET
+os.environ["EB_AGENT_ACCESS_TOKEN"] = GLOBAL_ERNIETOKEN
+erniebot.api_type = "aistudio"
+#erniebot.access_token = GLOBAL_ERNIETOKEN
