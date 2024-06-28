4851446141f04e29de351ee1e648a6c5f892007d
TheMasterWhite <2778178745@qq.com>
2024-06-28 19:37:50 +0800

    修正捕获错误过程

--- utils/LModel.py
+++ utils/LModel.py
@@ -3,6 +3,7 @@ from utils.FileProcess import *
 
 prevmsg = []
 
+
 class LModel:  # 大模型处理类
 
     @staticmethod
@@ -17,7 +18,6 @@ class LModel:  # 大模型处理类
         except Exception as e:
             return str(e)
 
-
     @staticmethod
     def GetResponseStream_String(Prompt):  # 流式获取推理结果，传入字符串，返回迭代器
         try:
@@ -30,7 +30,7 @@ class LModel:  # 大模型处理类
             for i in Response:
                 yield i
         except Exception as e:
-                return str(e)
+            return str(e)
 
     @staticmethod
     def GetResponse_List(ListPrompt):  # 获取推理结果，传入List，返回String
@@ -54,13 +54,13 @@ class LModel:  # 大模型处理类
             for i in Response.get_result():
                 yield i
         except Exception as e:
-                return str(e)
+            return str(e)
 
     @staticmethod
     def Translate(Tartext, LanCode=2):  # Tartext传入翻译目标字符串，TarLanguage传入int型目标语言代号,返回String
 
         try:
-            AbsPromptPath = FileProcess.AbsPath(Global_TranslationPath)
+            AbsPromptPath = FileProcess.AbsPath(GLOBAL_TranslationPath)
             Prompt = FileProcess.ReadTxt(AbsPromptPath)
             Code = {1: "中文", 2: "英语", 3: "日语", 4: "俄语", 5: "法语"}
             TarLanguage = Code.get(LanCode, "ErrorCode")
@@ -75,11 +75,10 @@ class LModel:  # 大模型处理类
         except Exception as e:
             return str(e)
 
-
     @staticmethod
     def Summary(Tartext):  # 精炼语言，传入目标句子，返回String
         try:
-            AbsPromptPath = FileProcess.AbsPath(Global_SummaryPath)
+            AbsPromptPath = FileProcess.AbsPath(GLOBAL_SummaryPath)
             Prompt = FileProcess.ReadTxt(AbsPromptPath)
             Prompt += Tartext
             return LModel.GetResponse_String(Prompt)
@@ -90,7 +89,7 @@ class LModel:  # 大模型处理类
     def Correct(Tartext):  # 句子纠错，Tartext传入目标字符串，OpeartionCode传入操作代码(int),返回String
 
         try:
-            AbsPromptPath = FileProcess.AbsPath(Global_CorrectPath)
+            AbsPromptPath = FileProcess.AbsPath(GLOBAL_CorrectPath)
             Prompt = FileProcess.ReadTxt(AbsPromptPath)
             Prompt += Tartext
             return LModel.GetResponse_String(Prompt)
@@ -100,7 +99,7 @@ class LModel:  # 大模型处理类
     @staticmethod
     def Polish(Tartext):  # 文章润色
         try:
-            AbsPromptPath = FileProcess.AbsPath(Global_PolishPath)
+            AbsPromptPath = FileProcess.AbsPath(GLOBAL_PolishPath)
             Prompt = FileProcess.ReadTxt(AbsPromptPath)
             Prompt += Tartext
             return LModel.GetResponse_String(Prompt)
@@ -121,8 +120,11 @@ class LModel:  # 大模型处理类
         })
         return Dialogue
 
+
 def Test():
-    LModel.GetResponse_String("我是一只雪狐！我要吃浆果！")
+    a = LModel.Correct("我是一只雪狐！我要吃浆果！")
+    print(a)
+
 
 if __name__ == '__main__':
     Test()
