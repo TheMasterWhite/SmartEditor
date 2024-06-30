2d8b68f606c6601cc5abf9c2b52bfb89b227e223
TheMasterWhite <2778178745@qq.com>
2024-06-30 22:41:09 +0800

    调用方式优化

--- utils/SModel.py
+++ utils/SModel.py
@@ -11,31 +11,31 @@ class SModel:  # 小模型应用接口类
         :return: access_token，或是None(如果错误)
         """
         url = "https://aip.baidubce.com/oauth/2.0/token"
-        params = {"grant_type": "client_credentials", "client_id": Global_Baidu_AK, "client_secret": Global_Baidu_SK}
+        params = {"grant_type": "client_credentials", "client_id": GLOBAL_Baidu_AK, "client_secret": GLOBAL_Baidu_SK}
         return str(requests.post(url, params=params).json().get("access_token"))
 
     @staticmethod
-    def GetOcrResult(FilePath, FileType=0, SavePath="Saves/OcrResult"):
+    def GetOcrResult(FilePath, FileType=1, SavePath="Saves/OcrResult"):
         # 获取OCR识别内容，传入相对文件地址、文件类型和JSON文件保存地址，0为PDF，1为图片,返回解析文本
         try:
             ABSPath = FileProcess.AbsPath(FilePath)
             Base64File = FileProcess.Base64(ABSPath)
             # 设置鉴权头
             headers = {
-                "Authorization": f"token {Global_ERNIEToken}",
+                "Authorization": f"token {GLOBAL_ERNIETOKEN}",
                 "Content-Type": "application/json"
             }
             # 请求头
             Payload = {
                 "file": Base64File,  # Base64编码的文件内容或者文件链接
                 "fileType": FileType,  # 本地文件类型，0:pdf,1:图片，此参数在上传本地文件时必须设置，使用文件链接时可省略
-                "aistudioToken": Global_ERNIEToken,
+                "aistudioToken": GLOBAL_ERNIETOKEN,
                 "inferenceParams": {
                     "maxLongSide": 960  # 文本检测长边的最大值，当大分辨率图片漏检严重时，可调大该值
                 }
             }
             # 获取解析结果
-            OcrResponse = requests.post(Global_OCRURL, json=Payload, headers=headers)
+            OcrResponse = requests.post(GLOBAL_OCRURL, json=Payload, headers=headers)
             OcrResponseData = OcrResponse.json()
 
             # JSON文件操作
@@ -69,7 +69,6 @@ class SModel:  # 小模型应用接口类
             "format": FileExtension,
             "pid": Code[Language],
             "rate": 16000,
-            "pid": 1737
         })
         headers = {
             'Content-Type': 'application/json',
@@ -80,6 +79,7 @@ class SModel:  # 小模型应用接口类
 
         # 上传信息
         CreateData = json.loads(CreateResponse.text)
+        print(CreateData)
         CreateTask = CreateData["task_id"]
         # print("task id:" + task)
         QuertPayload = json.dumps({"task_ids": [CreateTask]})
@@ -89,7 +89,7 @@ class SModel:  # 小模型应用接口类
             QueryData = json.loads(QueryResponse.text)
             # 获取状态
             Status = QueryData['tasks_info'][0]['task_status']
-            # print(Status)
+            print(Status)
 
             if (Status == "Success"):
                 return QueryData['tasks_info'][0]['task_result']
@@ -118,16 +118,16 @@ class SModel:  # 小模型应用接口类
             boxResult = responseData["result"]["bboxResult"]
 
             # 保存预测结果json文件
-            savePath = FileProcess.SaveWithTime(fileName="Result",
-                                                tarPath=SavePath,
-                                                fileExtension="json")
+            savePath = FileProcess.SaveWithTime(FileName="Result",
+                                                TarPath=SavePath,
+                                                FileExtension="json")
             with open(savePath, "w") as f:
                 json.dump(boxResult, f)
 
             # 处理识别结果图片并保存
-            savePath = FileProcess.SaveWithTime(fileName="Result",
-                                                tarPath=SavePath,
-                                                fileExtension="jpg")
+            savePath = FileProcess.SaveWithTime(FileName="Result",
+                                                TarPath=SavePath,
+                                                FileExtension="jpg")
             imageBase64 = responseData["result"]["image"]
             imageBytes = base64.b64decode(imageBase64)
             imageArray = np.frombuffer(imageBytes, dtype=np.uint8)
