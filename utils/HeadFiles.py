723426bcdf95d8c70239951a3ee8814984354e80
TheMasterWhite <2778178745@qq.com>
2024-06-30 22:42:07 +0800

    删掉提示词文件路径

--- utils/HeadFiles.py
+++ utils/HeadFiles.py
@@ -2,6 +2,8 @@ import datetime
 import os
 import time
 from configparser import ConfigParser
+import numpy as np
+import cv2
 import erniebot
 import gradio as gr
 import asyncio
@@ -16,23 +18,30 @@ from oss2.credentials import EnvironmentVariableCredentialsProvider
 from pathlib import Path
 
 
-def ReadConfigFile():
-    # 读取配置文件
-    Config = ConfigParser()
-    Config.optionxform = str
-    Config.read("Config.cfg")
-    # 遍历配置文件中的所有选项，创建全局变量
+class HeadFiles:
 
-    global_vars = dict(Config["TOKENS"])
-    for key, value in global_vars.items():
-        globals()[key] = value
+    def ReadConfigFile():
+        # 读取配置文件
+        Config = ConfigParser()
+        Config.optionxform = str
+        Config.read("Config.cfg")
+        # 遍历配置文件中的所有选项，创建全局变量
+        global_vars = dict(Config["TOKENS"])
+        for key, value in global_vars.items():
+            globals()[key] = value
+
+    def InitToken():  # 初始化环境变量
+
+        os.environ['OSS_ACCESS_KEY_ID'] = OSS_ACCESS_KEY_ID
+        os.environ['OSS_ACCESS_KEY_SECRET'] = OSS_ACCESS_KEY_SECRET
+        os.environ["EB_AGENT_ACCESS_TOKEN"] = GLOBAL_ERNIETOKEN
+        erniebot.api_type = "aistudio"
+        erniebot.access_token = GLOBAL_ERNIETOKEN
+
+    @staticmethod
+    def InitProcess():
+        HeadFiles.ReadConfigFile()
+        HeadFiles.InitToken()
 
 
-ReadConfigFile()
-
-
-os.environ['OSS_ACCESS_KEY_ID'] = OSS_ACCESS_KEY_ID
-os.environ['OSS_ACCESS_KEY_SECRET'] = OSS_ACCESS_KEY_SECRET
-os.environ["EB_AGENT_ACCESS_TOKEN"] = GLOBAL_ERNIETOKEN
-erniebot.api_type = "aistudio"
-erniebot.access_token = GLOBAL_ERNIETOKEN
+HeadFiles.InitProcess()
