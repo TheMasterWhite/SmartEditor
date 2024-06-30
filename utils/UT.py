1311acc7077eaacc7421294c531267e80f804722
TheMasterWhite <2778178745@qq.com>
2024-06-30 22:40:44 +0800

    新增单元测试

--- utils/UT.py
+++ utils/UT.py
@@ -1,18 +1,28 @@
-from paddlespeech.server.bin.paddlespeech_client import ASRClientExecutor
-from paddle import *
-import torch
+from HeadFiles import *
+from SModel import *
+from LModel import *
 
 
-def main():
-    audio = "resources/zh.wav"
-    asr = ASRExecutor()
-    result = asr(audio_file=audio, model='conformer_online_wenetspeech')
-    print(result)
+def LModelTest():
+    a = LModel.Translate("我是雪狐！快夸我可爱！")
+    b = LModel.Correct("我是雪狐！快夸我可爱！")
+    c = LModel.Summary("我是雪狐！快夸我可爱！")
+    d = LModel.Polish("我是雪狐！快夸我可爱！")
+    print(a)
+    print(b)
+    print(c)
+    print(d)
 
-def test():
-    tts = TTSExecutor()
-    tts(text="今天天气十分不错。", output="output.wav")
-
+def SModelTest():
+    # path = "resources/house.jpeg"
+    # a = SModel.GetTarDetectResult(FilePath=path)
+    ocrpath = "resources/Poster.jpg"
+    # b = SModel.GetOcrResult(ocrpath)
+    # bb = SModel.GetOcrResult("resources/作文.pdf",0)
+    #c = SModel.GetSTTResult("resources/英文语音测试.mp3",FileExtension="mp3",Language="English")
+    d = SModel.GetSTTResult("resources/沁园春雪.mp3",FileExtension="mp3",Language="Chinese")
+    #print(c)
+    print(d)
 
 if __name__ == '__main__':
-    pass
+    SModelTest()
