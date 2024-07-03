eaa0dffd23624f952e074a08d0b2b1a6384af642
TheMasterWhite <2778178745@qq.com>
2024-07-03 22:41:34 +0800

    完善了小模型单元测试

--- utils/UT.py
+++ utils/UT.py
@@ -1,6 +1,7 @@
 from HeadFiles import *
 from SModel import *
 from LModel import *
+import pprint
 
 
 def LModelTest():
@@ -15,14 +16,23 @@ def LModelTest():
 
 
 def SModelTest():
-    # path = "resources/house.jpeg"
-    # a = SModel.GetTarDetectResult(FilePath=path)
-    ocrpath = "resources/Poster.jpg"
-    # bb = SModel.GetOcrResult("resources/作文.pdf",0)
-    # c = SModel.GetSTTResult("resources/英文语音测试.mp3",FileExtension="mp3",Language="English")
-    d = SModel.GetSTTResult("resources/沁园春雪.mp3", FileExtension="mp3", Language="Chinese")
-    # print(c)
+    housePath = "resources/house.jpeg"
+    posterPath = "resources/Poster.jpg"
+    chineseSTTPath = "resources/沁园春雪.mp3"
+    englishSTTPath = "resources/英文语音测试.mp3"
+    pdfPath = "resources/作文.pdf"
+
+    a = SModelInterface.GetDocOcrResult(FilePath = pdfPath, FileType = "PDF")
+    b = SModelInterface.GetRawOcrResult(FilePath = posterPath)
+    c = SModelInterface.GetTarDetectResult(FilePath = housePath)
+    d = SModelInterface.GetSTTResult(FilePath = englishSTTPath, FileExtension = "mp3", Language = "English")
+    e = SModelInterface.GetSTTResult(FilePath = chineseSTTPath, FileExtension = "mp3", Language = "Chinese")
+
+    print(a)
+    print(b)
+    print(c)
     print(d)
+    print(e)
 
 
 def Test():
@@ -32,6 +42,7 @@ def Test():
                  {"role": "assistant", "content": "我是第一轮大模型回复内容"},
                  {"role": "user", "content": "功能提示词(静态) + 用户编辑器文本(动态)"},
                  {"role": "assistant", "content": "我是第二轮大模型回复内容"}]
+
 
 def FileIterator():
     for i in range(1, 7):
@@ -43,15 +54,20 @@ def FileIterator():
         else:
             print(f" {filePath} 不存在。")
 
+
 def SaveMoney():
     result = ""
     prompt = "你是一个聊天记录分析助手，下文冒号后面是一串由OCR识别到的微信聊天群消息，你需要将其内容尽可能详细地复述出来："
     for i in FileIterator():
-        content = SModel.GetOcrResult(FilePath=i, FileType=1)
+        content = SModelInterface.GetRawOCRResult(FilePath = i)
         result += content
     prompt += result
     result = LLMInterface.GetResponse_String(prompt)
     print(result)
 
+
 if __name__ == '__main__':
-    LModelTest()
+    posterPath = "resources/Poster.jpg"
+    print(SModelInterface.GetRawOcrResult(FilePath = posterPath))
+
+    # GetTarDetectResult()需要增加json解析器
