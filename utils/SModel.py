33ed9efcaa33d5dd1f7d3a150178898bf4e15064
TheMasterWhite <2778178745@qq.com>
2024-07-03 22:43:52 +0800

    新增RawOcr调用接口，传输基本功能接口与高级功能接口改为继承封装，更改类名

--- utils/SModel.py
+++ utils/SModel.py
@@ -11,11 +11,11 @@ class SModelBasic:  # 小模型应用接口类
 
         url = "https://aip.baidubce.com/oauth/2.0/token"
         params = {"grant_type": "client_credentials", "client_id": GLOBAL_Baidu_AK, "client_secret": GLOBAL_Baidu_SK}
-        return str(requests.post(url, params=params).json().get("access_token"))
+        return str(requests.post(url, params = params).json().get("access_token"))
 
 
     @staticmethod
-    def GetDocOcrResult(FilePath, FileType="IMG", SavePath="Saves/OcrResult"):  # 获取文档抽取OCR模型识别内容，
+    def GetDocOcrResult(FilePath, FileType = "IMG", SavePath = "Saves/OcrResult"):  # 获取文档抽取OCR模型识别内容，
         # 传入相对文件地址、文件类型和JSON文件保存地址，文件类型为"IMG"或"PDF",返回解析文本
         fileCode = {"PDF": 0, "IMG": 1}
         try:
@@ -34,9 +34,9 @@ class SModelBasic:  # 小模型应用接口类
                 }
             }
             # 获取解析结果
-            ocrResponse = requests.post(GLOBAL_DOC_OCRURL, json=Payload, headers=headers)
+            ocrResponse = requests.post(GLOBAL_DOC_OCRURL, json = Payload, headers = headers)
             ocrResponseData = ocrResponse.json()
-            saveData = json.dumps(ocrResponseData, ensure_ascii=False, indent=4)
+            saveData = json.dumps(ocrResponseData, ensure_ascii = False, indent = 4)
 
             # JSON文件操作
             text = ocrResponseData['result']['tableOcrResult']['text_result']
@@ -44,10 +44,10 @@ class SModelBasic:  # 小模型应用接口类
             resultText = max(text, tableText)
 
             # 处理文件路径
-            savePath = FileProcess.SaveWithTime(FileName="DocResult",
-                                                TarPath=SavePath,
-                                                FileExtension="json")
-            with open(savePath, 'w', encoding='utf-8') as f:
+            savePath = FileProcess.SaveWithTime(FileName = "DocResult",
+                                                TarPath = SavePath,
+                                                FileExtension = "json")
+            with open(savePath, 'w', encoding = 'utf-8') as f:
                 f.write(saveData)
             return resultText
 
@@ -56,7 +56,7 @@ class SModelBasic:  # 小模型应用接口类
 
 
     @staticmethod
-    def GetSTTResult(FilePath, FileExtension, Language="Chinese", SavePath="Saves/SttResult"):  # Chinese或English
+    def GetSTTResult(FilePath, FileExtension, Language = "Chinese", SavePath = "Saves/SttResult"):  # Chinese或English
         # 获取语音转文字结果，传入相对路径、扩展名和语言，返回转写结果String并保存到SavePath
 
         AbsPath = FileProcess.AbsPath(FilePath)
@@ -76,7 +76,7 @@ class SModelBasic:  # 小模型应用接口类
             'Content-Type': 'application/json',
             'Accept': 'application/json'
         }
-        CreateResponse = requests.request("POST", CreatUrl, headers=headers, data=payload)
+        CreateResponse = requests.request("POST", CreatUrl, headers = headers, data = payload)
         # print(Response.text)
 
         # 上传信息
@@ -86,17 +86,17 @@ class SModelBasic:  # 小模型应用接口类
         QuertPayload = json.dumps({"task_ids": [CreateTask]})
 
         while (1):
-            queryResponse = requests.request("POST", QueryUrl, headers=headers, data=QuertPayload)
+            queryResponse = requests.request("POST", QueryUrl, headers = headers, data = QuertPayload)
             queryData = queryResponse.json()
-            saveData = json.dumps(queryData, ensure_ascii=False, indent=4)
+            saveData = json.dumps(queryData, ensure_ascii = False, indent = 4)
             # 获取状态
             status = queryData['tasks_info'][0]['task_status']
 
             if (status == "Success"):
 
-                savePath = FileProcess.SaveWithTime(FileName="Result",
-                                                    TarPath=SavePath,
-                                                    FileExtension="json")
+                savePath = FileProcess.SaveWithTime(FileName = "Result",
+                                                    TarPath = SavePath,
+                                                    FileExtension = "json")
                 with open(savePath, "w") as f:
                     f.write(saveData)
                 return queryData['tasks_info'][0]['task_result']['result']
@@ -106,7 +106,7 @@ class SModelBasic:  # 小模型应用接口类
 
 
     @staticmethod
-    def GetTarDetectResult(FilePath, SavePath="Saves/TarResult"):
+    def GetTarDetectResult(FilePath, SavePath = "Saves/TarResult"):
         # 获取目标检测结果，传入文件地址返回结果String并保存json到SavePath
 
         API_URL = "https://mas5g0dfrereq9ta.aistudio-hub.baidu.com/objectdetection"
@@ -123,25 +123,25 @@ class SModelBasic:  # 小模型应用接口类
                 "image": base64File
             }
             # 请求结果并解析
-            response = requests.post(API_URL, json=payload, headers=headers)
+            response = requests.post(API_URL, json = payload, headers = headers)
             responseData = response.json()
-            saveData = json.dumps(responseData, ensure_ascii=False, indent=4)
+            saveData = json.dumps(responseData, ensure_ascii = False, indent = 4)
 
             # 保存预测结果json文件
-            savePath = FileProcess.SaveWithTime(FileName="Result",
-                                                TarPath=SavePath,
-                                                FileExtension="json")
+            savePath = FileProcess.SaveWithTime(FileName = "Result",
+                                                TarPath = SavePath,
+                                                FileExtension = "json")
             with open(savePath, "w") as f:
                 f.write(saveData)
 
             # 处理识别结果图片并保存
-            savePath = FileProcess.SaveWithTime(FileName="Result",
-                                                TarPath=SavePath,
-                                                FileExtension="jpg")
+            savePath = FileProcess.SaveWithTime(FileName = "Result",
+                                                TarPath = SavePath,
+                                                FileExtension = "jpg")
             imageBase64 = responseData["result"]["image"]
             imageBytes = base64.b64decode(imageBase64)
-            imageArray = np.frombuffer(imageBytes, dtype=np.uint8)
-            predictedImage = cv2.imdecode(imageArray, flags=cv2.IMREAD_COLOR)
+            imageArray = np.frombuffer(imageBytes, dtype = np.uint8)
+            predictedImage = cv2.imdecode(imageArray, flags = cv2.IMREAD_COLOR)
             cv2.imwrite(savePath, predictedImage)
             return "Need to be added json function"
 
@@ -150,7 +150,7 @@ class SModelBasic:  # 小模型应用接口类
 
 
     @staticmethod
-    def GetRawOcrResult(FilePath, SavePath="Saves/OcrResult"):
+    def GetRawOcrResult(FilePath, SavePath = "Saves/OcrResult"):
         # 获取RawOCR识别结果，传入文件地址返回结果String并保存json到SavePath
 
         try:
@@ -163,12 +163,12 @@ class SModelBasic:  # 小模型应用接口类
             payload = {
                 "image": base64File  # Base64编码的文件内容或者文件链接
             }
-            savePath = FileProcess.SaveWithTime(FileName="RawResult",
-                                                TarPath=SavePath,
-                                                FileExtension="json")
-            response = requests.post(url=GLOBAL_RAW_OCRURL, json=payload, headers=headers)
+            savePath = FileProcess.SaveWithTime(FileName = "RawResult",
+                                                TarPath = SavePath,
+                                                FileExtension = "json")
+            response = requests.post(url = GLOBAL_RAW_OCRURL, json = payload, headers = headers)
             responseData = response.json()
-            saveData = json.dumps(responseData, ensure_ascii=False, indent=4)
+            saveData = json.dumps(responseData, ensure_ascii = False, indent = 4)
             result = [i['text'] for i in responseData['result']['texts']]
             resultText = "".join(result)
 
@@ -188,8 +188,7 @@ class SModelInterface(SModelBasic):
 
 def test():
     path = "resources/house.jpeg"
-    a = SModelBasic.GetTarDetectResult(FilePath=path)
-    print(a)
+    a = SModelBasic.GetTarDetectResult(FilePath = path)
 
 
 if __name__ == '__main__':
