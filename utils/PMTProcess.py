2c6f3c9b1b2be99a93ab289141880dcff1c44edc
TheMasterWhite <2778178745@qq.com>
2024-07-03 22:40:50 +0800

    优化了代码风格

--- utils/PMTProcess.py
+++ utils/PMTProcess.py
@@ -7,18 +7,20 @@ class GetPrompt:  # 获取Prompt的单例模式类
     instance = None  # 存储实例
     data = None  # 存储数据
 
+
     def __new__(cls, *args, **kwargs):  # 创建实例
         if not cls.instance:
             cls.instance = super(GetPrompt, cls).__new__(cls, *args, **kwargs)
-            with open("Prompts.json", "r", encoding="utf-8") as f:
+            with open("Prompts.json", "r", encoding = "utf-8") as f:
                 cls.data = json.load(f)
         return cls.instance
+
 
     def Data(self):
         return self.data
 
 
-class KnowledgeLib:
+class KnowledgeLib:  # 知识，用于维护用户知识库
 
     @staticmethod
     def InitLib(KnowledgeText):  # 载入用户知识库，传入知识库字符串返回初始化后的列表
@@ -27,4 +29,5 @@ class KnowledgeLib:
         prompt += KnowledgeText
         parameter = [{"role": "user", "content": prompt},
                      {"role": "assistant", "content": "明白"}]
+
         return parameter
