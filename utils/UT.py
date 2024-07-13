526cacc7e74fbc04f92be3d18e777fc7e8865347
TheMasterWhite <2778178745@qq.com>
2024-07-13 12:56:50 +0800

    Update UT.py

--- utils/UT.py
+++ utils/UT.py
@@ -1,8 +1,7 @@
-from utils.HeadFiles import *
 from utils.SModel.OCR import *
 from utils.SModel.TarDetect import *
 from utils.SModel.STT import *
-from LModel import *
+from utils.LModel.Interface import *
 
 
 def LModelTest():
@@ -23,28 +22,27 @@ def SModelTest():
     englishSTTPath = "resources/英文语音测试.mp3"
     pdfPath = "resources/作文.pdf"
 
-    # a = TarInterface.GetResult(housePath)
-    # b = OCRInterface.Raw(posterPath)
-    # c = OCRInterface.Doc(posterPath)
-    # d = OCRInterface.Doc(pdfPath,FileType = "PDF")
+    a = TarInterface.GetResult(housePath)
+    b = OCRInterface.Raw(posterPath)
+    c = OCRInterface.Doc(posterPath)
+    d = OCRInterface.Doc(pdfPath, FileType = "PDF")
     e = STTInterface.GetResult(chineseSTTPath, Language = "Chinese", FileExtension = "mp3")
     f = STTInterface.GetResult(englishSTTPath, Language = "English", FileExtension = "mp3")
 
-    # print("a = " + a)
-    # print("b = " + b)
-    # print("c = " + c)
-    # print("d = " + d)
-    print(e)
-    print(f)
+    print("a = " + a)
+    print("b = " + b)
+    print("c = " + c)
+    print("d = " + d)
+    print("e = " + e)
+    print("f = " + f)
 
 
 def Test():
-    a = GetPrompt().Data()["FunctionPrompt"]["Translate"]
-    print(a + '1')
-    Parameter = [{"role": "user", "content": "知识库载入提示词(静态) + 用户知识库(动态)"},
-                 {"role": "assistant", "content": "我是第一轮大模型回复内容"},
-                 {"role": "user", "content": "功能提示词(静态) + 用户编辑器文本(动态)"},
-                 {"role": "assistant", "content": "我是第二轮大模型回复内容"}]
+    a = input()
+    absPath = FileProcess.AbsPath("resources/亚托莉.txt")
+    lib = FileProcess.ReadTxt(absPath)
+    result = LLMInterface.Check_String(a, lib)
+    print(result)
 
 
 def FileIterator():
@@ -62,12 +60,11 @@ def SaveMoney():
     result = ""
     prompt = "你是一个聊天记录分析助手，下文冒号后面是一串由OCR识别到的微信聊天群消息，你需要将其内容尽可能详细地复述出来："
     for i in FileIterator():
-        content = SModelInterface.GetRawOCRResult(FilePath = i)
+        content = OCRInterface.GetRawOCRResult(FilePath = i)
         result += content
     prompt += result
     result = LLMInterface.GetResponse_String(prompt)
     print(result)
 
-
-if __name__ == '__main__':
-    SModelTest()
+if __name__ == "__main__":
+    pass
