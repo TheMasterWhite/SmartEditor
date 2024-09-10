eedca89da062276e034ff677ef5cdaed53e9b614
TheMasterWhite <2778178745@qq.com>
2024-09-10 19:54:35 +0800

    更新

--- utils/SModel/STT.py
+++ utils/SModel/STT.py
@@ -74,18 +74,20 @@ class TaskThread(threading.Thread):
         self.taskQueue.put(TaskId)
         self.fileIdList[TaskId] = FileName
 
+
 QuerySTTThread = TaskThread()
 QuerySTTThread.start()
+
 
 class STTInterface:  # 小模型应用接口类
 
     # 创建音频转写任务
     @staticmethod
-    def CreateTask(FileName, Language = "Chinese"):
+    def CreateTask(FullFileName, Language = "Chinese"):
 
         try:
-            fileExtension = Tools.GetExtension(FileName)
-            fileName = Tools.GetFileName(FileName)
+            fileExtension = Tools.GetExtension(FullFileName)
+            fileName = Tools.GetFileName(FullFileName)
             # 将文件上传到阿里云并获取外链
             ossPath = OSSProcess.UploadFile(FileName = fileName,
                                             FileExtension = fileExtension)
@@ -123,12 +125,12 @@ class STTInterface:  # 小模型应用接口类
             fileName = Tools.GetFileName(FullFileName)
 
             # 视频文件转wav再STT处理
-            if FileExtension in ["mp4"]:
+            if fileExtension in ["mp4"]:
                 FileProcess.ConvertToWav(FileName = FullFileName,
                                          FileExtension = fileExtension)
                 # 发起STT服务调用
                 fileName_Wav = fileName + ".wav"
-                taskId = STTInterface.CreateTask(FileName = fileName_Wav)
+                taskId = STTInterface.CreateTask(FullFileName = fileName_Wav)
                 # 加入轮询队列
                 QuerySTTThread.PutTaskId(FileName = fileName_Wav,
                                          TaskId = taskId,
@@ -137,7 +139,7 @@ class STTInterface:  # 小模型应用接口类
             # 音频文件，直接转文字处理
             elif fileExtension in ["wav", "mp3", "pcm", "m4a", "amr"]:
                 # 发起STT服务调用
-                taskId = STTInterface.CreateTask(FileName = FullFileName)
+                taskId = STTInterface.CreateTask(FullFileName = FullFileName)
                 # 加入轮询队列
                 QuerySTTThread.PutTaskId(FileName = FullFileName,
                                          TaskId = taskId,
@@ -151,6 +153,3 @@ class STTInterface:  # 小模型应用接口类
         except Exception as e:
             curTime = Tools.GetTime()
             logging.error(f"[{curTime}]Module:[STT_MainProcess]" + str(e))
-
-
-
