51a5581eb00970cde7880b560f7fac9ef4806e0d
TheMasterWhite <2778178745@qq.com>
2024-09-10 21:26:51 +0800

    更新

--- utils/SModel/STT.py
+++ utils/SModel/STT.py
@@ -2,7 +2,7 @@ from utils.Config.PMTProcess import *
 from Config import *
 from utils import Tools
 import queue, threading, time, logging
-from threading import Thread
+from TaskThread import *
 
 fileSavePath = GLOBAL_FileSavePath
 logging.basicConfig(filename = "Server/Log.log",
