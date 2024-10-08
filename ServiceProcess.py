3e865be07e106eac910a63843d1c3ea971ff6ea0
TheMasterWhite <2778178745@qq.com>
2024-10-08 16:44:45 +0800

    更新

--- ServiceProcess.py
+++ ServiceProcess.py
@@ -259,6 +259,9 @@ def DeleteFile():
 
             return jsonify(retObj)
 
+    except FileNotFoundError as e:
+        pass
+
     except Exception as e:
         curTime = Tools.GetTime()
         logging.error(f"[{curTime}]Module:[DeleteFile]" + str(e))
