5baceaafc5ba552cbaba83bf8b1bff65e470c932
TheMasterWhite <2778178745@qq.com>
2024-11-05 15:22:41 +0800

    更新

--- ServiceProcess.py
+++ ServiceProcess.py
@@ -576,14 +576,14 @@ def protected():
         # 从请求头中获取token
         token = request.headers.get("Authorization")
         curTime = Tools.GetTime()
-        # payload = jwt.decode(token, GLOBAL_JWT_KEY, algorithms = ["RS256"])
-        # username = payload["username"]
-        #
-        # conn = sqlite3.connect("UserInfo.db")
-        # cursor = conn.cursor()
-        # cursor.execute("SELECT username FROM users WHERE username = ?", (username,))
-        # result = cursor.fetchone()
-        # searchName = result[0] if result else None
+        payload = jwt.decode(token, GLOBAL_JWT_KEY, algorithms = ["RS256"])
+        username = payload["username"]
+
+        conn = sqlite3.connect("UserInfo.db")
+        cursor = conn.cursor()
+        cursor.execute("SELECT username FROM users WHERE username = ?", (username,))
+        result = cursor.fetchone()
+        searchName = result[0] if result else None
 
         retObj = {
             "statusCode": 1,
